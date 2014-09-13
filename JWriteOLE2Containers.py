faab70ec59cc51182a1f3fb73929288981a8d4cc
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2014-09-13 21:27:50 +1200

    [feature] Jython OLE2 container writer.

---
+++ JWriteOLE2Containers.py
@@ -0,0 +1,50 @@
+import os
+
+from jarray import zeros
+from java.io import FileOutputStream, FileInputStream, ByteArrayOutputStream
+
+from org.apache.poi.poifs.filesystem import NPOIFSFileSystem
+
+class WriteOLE2Containers():
+
+	def __debugfos__(fos, bufsize):
+		buf = zeros(bufsize, 'b')
+		fin.read(buf)
+		print buf
+
+	def writeContainer(self, containerfoldername, outputfolder):
+
+		written = False
+
+		#we have folder name, written earlier
+		#foldername is filename!!	
+		if os.path.isdir(containerfoldername):
+	
+			fname = outputfolder + '/' + containerfoldername.split('/',1)[1].replace('/', '')
+
+			fs = NPOIFSFileSystem()
+			root = fs.getRoot();
+
+			#triplet ([Folder], [sub-dirs], [files])
+			for folder, subs, files in os.walk(containerfoldername):
+				if subs != []:
+					#TODO: cant't yet write directories		
+					break
+				else:
+					for f in files:
+						fin = FileInputStream(folder + f)
+						if fin.getChannel().size() == 0:
+							fin.close()
+							written = False
+							break
+						else:
+							root.createDocument(f, fin)
+							fin.close()
+							written = True
+
+			if written == True:
+				fos = FileOutputStream(fname)
+				fs.writeFilesystem(fos);
+				fs.close()
+
+		return written
